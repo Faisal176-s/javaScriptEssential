5f3fcc6b094c6876b89ffc83c15e13e917ed5e0c
Faisal176-s <faisal.almugesib@gmail.com>
2024-03-21 03:16:01 +0300

    Json

---
+++ SampleFolder/JSON.js
@@ -0,0 +1,16 @@
+// JSON structure represented as a string
+const jsonString = '{"name": "John Doe", "age": 30, "isStudent": true}';
+// JavaScript object
+const personObject = {
+  name: "Jane Smith",
+  age: 25,
+  isStudent: false
+};
+
+// Parsing JSON string to JavaScript object
+const parsedJSON = JSON.parse(jsonString); //Changes the string that is written in JSON format to object
+console.log(parsedJSON); // Output: { name: 'John Doe', age: 30, isStudent: true }
+console.log(typeof parsedJSON); // Output: object
+//Directly Using JavaScript Object:
+console.log(personObject); // Output: { name: 'Jane Smith', age: 25, isStudent: false }
+console.log(typeof personObject); // Output: object
